52e5d9e0e2144047eb7595e910ea9a1f20cf5f17
diegorodd <dieegoa@icloud.com>
2022-10-05 22:49:08 -0400

    adding packages

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start()
   // integrate our Apollo server with the Express application as middleware
@@ -35,7 +34,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`)
-      // log where we can go to test our GQL API
+      
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`,
       )
